85a9d6e73bf69895f345b7e8713cb3b398988a6c
Ekekenta Zion <60361247+icode247@users.noreply.github.com>
2021-09-16 18:40:29 +0100

    Update Words.jsx

--- src/components/Words.jsx
+++ src/components/Words.jsx
@@ -1,15 +1,1 @@
-function Word(props) {
-  const searchList = document.querySelector(".listData");
-  searchList && searchList.insertAdjacentHTML("afterbegin",
-    `<li>
-        <h4>${props.data.title}</h4>
-        ${props.data.snippet}
-    </li>`);
-  return (
 
-    <div className="listData">
-
-    </div>
-  )
-}
-export default Word;
