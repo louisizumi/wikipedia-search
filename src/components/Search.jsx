c995b46e7c8ba7045b2b4af741e4ab5c028a1f38
Louis Drinkwater <Louis Drinkwater>
2022-05-02 15:48:21 +0100

    Add search feature

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,25 +1,67 @@
 import logo from '../logo.png'
+import { useState } from "react"
 
 function Search() {
-    return (
-        <div className="container">
-            <div className="header">
-                <div className="caption">
-                    <h4>WIKIPEDIA</h4>
-                    <p>The Free Encyclopedia</p>
-                </div>
-                <img src={logo} alt="" />
-            </div>
-            <div className="content">
-                <form action="">
-                    <input type="text" className="search"/>
-                </form>
-            </div>
-            <div className="word">
-                {/* Search result component */}
-            </div>
-        </div>
+  const [result, setResult] = useState({})
 
-    );
+  async function fetchData(val) {
+    const trimVal = val.trim().toLowerCase()
+    if (trimVal.length > 0) {
+      const api = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${trimVal}`;
+      const response = await fetch(api)
+      if (!response.ok) {
+         throw Error(response.statusText)
+      }
+      return await response.json()
+    }
+  }
+
+  async function handleSearch(e) {
+    const { value } = e.target
+    setResult({ result: await fetchData(value) })
+  }
+
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="caption">
+          <h4>WIKIPEDIA</h4>
+          <p>The Free Encyclopedia</p>
+        </div>
+        <img src={logo} alt="" />
+      </div>
+      <div className="content">
+        <form action="">
+          <input type="text" className="search" onKeyUp={handleSearch} />
+        </form>
+      </div>
+      <div className="word">
+        <ul>
+          {result.result
+            && result.result.query
+            && result.result.query.search.map(data =>
+              <Word key={data.id} data={data} />
+            )
+          }
+        </ul>
+      </div>
+    </div>
+  )
 }
-export default Search;
+
+function Word(props) {
+  const searchList = document.querySelector(".listData")
+
+  return (
+    <div className="listData">
+      {searchList && searchList.insertAdjacentHTML("afterbegin",
+        `<li>
+          <h4>${props.data.title}</h4>
+          ${props.data.snippet}
+        </li>`
+      )}
+    </div>
+  )
+}
+
+export default Search
